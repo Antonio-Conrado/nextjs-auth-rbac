7c745542b3725ddd1770a6ccc51f8ed038ae1c04
Antonio-Conrado <jconradolopez19@gmail.com>
2025-10-01 12:16:14 -0600

    Added middleware to handle auth redirects for public and private routes

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getTokens } from "./lib/const/cookies";
+
+const publicRoutes = [
+  "/login",
+  "/sign-up",
+  "/forgot-password",
+  "/confirm-account",
+  "/not-found",
+];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const { accessToken } = await getTokens();
+
+  if (!publicRoutes.includes(path) && !accessToken) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  if (isPublicRoute && accessToken) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
