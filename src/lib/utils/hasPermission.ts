fc1b96b3a2e1d0605c98eae43ecabd3e36acb75f
Antonio-Conrado <jconradolopez19@gmail.com>
2025-10-16 11:44:00 -0600

    Removed console; added hasPermission helper using useAppStore

--- src/lib/utils/hasPermission.ts
+++ src/lib/utils/hasPermission.ts
@@ -2,6 +2,5 @@ import { useAppStore } from "@/store/useStore";
 
 export function hasPermission(permission: string): boolean {
   const { user } = useAppStore.getState();
-  console.log(user?.role.permissions);
   return !!user?.role.permissions.find((p) => p.name === permission);
 }
