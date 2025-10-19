829260de996674977f0a9decec923ce82665d0b3
Antonio-Conrado <jconradolopez19@gmail.com>
2025-10-19 10:14:30 -0600

    Added super admin full access check to hasPermission function

--- src/lib/utils/hasPermission.ts
+++ src/lib/utils/hasPermission.ts
@@ -2,5 +2,7 @@ import { useAppStore } from "@/store/useStore";
 
 export function hasPermission(permission: string): boolean {
   const { user } = useAppStore.getState();
-  return !!user?.role.permissions.find((p) => p.name === permission);
+  return !!user?.role.permissions.find(
+    (p) => p.name === permission || p.name === "super.admin.fullAccess"
+  );
 }
